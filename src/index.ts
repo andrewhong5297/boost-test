6aded595b4d6ce8e2f0d86f3fa034c9948ec68d0
mmackz <matthewmackenz@gmail.com>
2025-07-02 20:52:40 -0700

    feat: add boost creation with managed budget functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { baseSepolia } from "viem/chains";
 import { account, baseSepoliaClient, core, walletClient } from "./config";
-import { getTransparentBudget } from "./utils/budget";
+import { getOrCreateBudget, getTransparentBudget, transferToBudget } from "./utils/budget";
 import { eventActionPayload } from "./utils/eventAction";
 import { erc20Abi, parseEventLogs, parseUnits } from "viem";
 import { boostCoreAbi, StrategyType } from "@boostxyz/sdk";
@@ -18,7 +18,7 @@ const incentivePayload = {
   strategy: StrategyType.POOL
 };
 
-const main = async () => {
+const createBoostWithTransparentBudget = async () => {
   console.log("Starting Boost Deployment...");
 
   const transparentBudget = await getTransparentBudget(baseSepolia.id);
@@ -94,4 +94,66 @@ const main = async () => {
   console.log(`https://sepolia.basescan.org/tx/${hash}`);
 };
 
-main();
+const createBoostWithManagedBudget = async () => {
+  const managedBudget = await getOrCreateBudget(account);
+
+  // make sure the budget has enough funds
+  const balance = await managedBudget.available(
+    "0x036cbd53842c5426634e7929541ec2318f3dcf7e"
+  ); // USDC (Base Sepolia)
+
+  if (balance < parseUnits("1.1", 6)) {
+    await transferToBudget(
+      managedBudget,
+      "0x036cbd53842c5426634e7929541ec2318f3dcf7e",
+      parseUnits("1.1", 6) - balance
+    );
+  }
+
+  const action = core.EventAction(eventActionPayload);
+  const incentive = core.ERC20Incentive(incentivePayload);
+
+  // Allowlist is open to all addresses.
+  const allowList = core.SimpleDenyList({
+    owner: account.address,
+    denied: []
+  });
+  const validator = core.LimitedSignerValidator({
+    signers: [signers.staging], // use production signer for mainnet
+    validatorCaller: core.assertValidAddress(), // address for BoostCore (https://github.com/boostxyz/boost-protocol/blob/c638b3f599c10e3f6cab7152849ddae612f2bd26/packages/evm/deploys/84532.json#L3)
+    maxClaimCount: 1 // allows for only 1 claim per address
+  });
+
+  console.log("Creating Boost...");
+  // raw version returns the hash instead of the boost object
+  const { hash } = await core.createBoostRaw({
+    budget: managedBudget,
+    action,
+    incentives: [incentive],
+    allowList,
+    validator,
+    owner: account.address
+  });
+
+  const boostReceipt = await baseSepoliaClient.waitForTransactionReceipt({ hash });
+
+  if (boostReceipt.status === "reverted") {
+    throw new Error("Boost Deployment failed");
+  }
+
+  console.log("Boost Deployment successful");
+
+  const logs = parseEventLogs({
+    abi: boostCoreAbi,
+    eventName: "BoostCreated",
+    logs: boostReceipt.logs
+  });
+
+  const boostId = logs[0].args.boostId;
+
+  console.log("Boost ID:", boostId);
+  console.log(`https://sepolia.basescan.org/tx/${hash}`);
+};
+
+createBoostWithTransparentBudget();
+// createBoostWithManagedBudget();
